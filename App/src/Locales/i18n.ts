9367d27319efee2f81dc36c95d41696bf3912246
szkj-web-he <xuejie.he@datareachable.com>
2022-10-20 10:08:35 +0800

    Update i18n.ts

--- App/src/Locales/i18n.ts
+++ App/src/Locales/i18n.ts
@@ -4,10 +4,11 @@
  * author: Frank
  * lastModify: Frank 2020-07-21
  */
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import translationCn from "./Translations/cn/translation.json";
-import translationEn from "./Translations/en/translation.json";
+import { getLanguage } from '@datareachable/dr_front_componentlibrary';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationCn from './Translations/cn/translation.json';
+import translationEn from './Translations/en/translation.json';
 
 const resources = {
     en: {
@@ -17,18 +18,20 @@ const resources = {
         translation: translationCn,
     },
 };
-
-void i18n
-    // pass the i18n instance to react-i18next.
-    .use(initReactI18next)
-    // init i18next
-    // for all options read: https://www.i18next.com/overview/configuration-options
-    .init({
-        resources,
-        lng: "en",
-        interpolation: {
-            escapeValue: false, // not needed for react!!
-        },
-    });
+void (async () => {
+    const lang = await getLanguage();
+    void i18n
+        // pass the i18n instance to react-i18next.
+        .use(initReactI18next)
+        // init i18next
+        // for all options read: https://www.i18next.com/overview/configuration-options
+        .init({
+            resources,
+            lng: lang ?? 'en',
+            interpolation: {
+                escapeValue: false, // not needed for react!!
+            },
+        });
+})();
 
 export default i18n;
